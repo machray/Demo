fbb9079078e2ab0e644e27bf8494d02a2b89a3fb
machray <machray505@hotmail.com>
2015-10-01 13:37:54 -0700

    Code to get system info

---
+++ src/com/cooksys/cornerstone/GetUname.java
@@ -0,0 +1,28 @@
+package com.cooksys.cornerstone;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GetUname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Runtime r = Runtime.getRuntime();
+		Process p;
+		try {
+			p = r.exec("uname -a");
+			p.waitFor();
+			BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line = "";
+	
+			while ((line = b.readLine()) != null) {
+				System.out.println(line);
+			}
+	
+			b.close();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
